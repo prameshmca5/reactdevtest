0d91beacf9230f3882c3f2f8ba18ac7d4433064d
Ramesh <rameshkp2@gmail.com>
2020-03-21 00:34:14 +0800

    Add and Remove Data Content.

--- src/components/Buger/BuildControllers/BuildController/BuildController.js
+++ src/components/Buger/BuildControllers/BuildController/BuildController.js
@@ -4,9 +4,10 @@ import "./BuildController.css";
 const BuildController = (props) => {
     return(
         <div className="BuildControl">
+
             <div className="Label">{props.label}</div>
             <div>
-                <button className="Less">Less</button>
+                <button className="Less" onClick={props.removeInheritant} disabled={props.disabledInfo}>Less</button>
 
             </div>
             <div><button className="More" onClick={props.addBurgercnt}>More</button></div>
--- src/components/Buger/BuildControllers/BuildControllers.js
+++ src/components/Buger/BuildControllers/BuildControllers.js
@@ -12,13 +12,17 @@ const control = [
 
 const BuildControllers = (props) => {
     return(
+
         <div className="BuildControls">
+            <div><p>Total Burger Price: {props.price.toFixed(2)}</p></div>
             {
                 control.map(ctrl=> {
                     return <BuildController
                         key={ctrl.label}
                         label={ctrl.label}
                         addBurgercnt = {() => props.addInherdiant(ctrl.type)}
+                        removeInheritant = { () => props.removeInheritant(ctrl.type)}
+                        disabledInfo ={props.disabled[ctrl.type]}
                     />
                 })
             }
